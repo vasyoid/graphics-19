91f0a1b7f557e6feac103c24899736a33eab0568
vasyoid <vasyoid@gmail.com>
2019-11-25 04:44:01 +0300

    Refactor to GLUT

--- 3d-model/3d-model.py
+++ 3d-model/3d-model.py
@@ -1,15 +1,14 @@
 import itertools
 import time
 from math import sin, cos
-
-import pygame
-from pygame.locals import *
-
 from OpenGL.GL import *
 from OpenGL.GLU import *
+from OpenGL.GLUT import *
 
 import tinyobjloader
 import numpy as np
+
+ESC_KEY = b'\x1b'
 
 WINDOW_HEIGHT = 900
 WINDOW_WIDTH = 1200
@@ -24,25 +23,10 @@ CAMERA_POSITION = (2, 2, 2)
 ZOOM_FACTOR = 1.1
 ROTATION_FACTOR = 0.2
 
-SCROLL_UP = 5
+SCROLL_UP = 3
 SCROLL_DOWN = 4
 
 COLOR_BLACK = (0, 0, 0, 0)
-
-
-def group(iterator, count):
-    itr = iter(iterator)
-    while True:
-        yield tuple([itr.__next__() for _ in range(count)])
-
-
-def draw(vertices, normals, indices, n_indices):
-    glBegin(GL_TRIANGLES)
-    for inds, n_inds in zip(group(indices, 3), group(n_indices, 3)):
-        for ind, n_ind in zip(inds, n_inds):
-            glNormal3f(normals[3 * n_ind], normals[3 * n_ind + 1], normals[3 * n_ind + 2])
-            glVertex3f(vertices[3 * ind], vertices[3 * ind + 1], vertices[3 * ind + 2])
-    glEnd()
 
 
 def normalize(v):
@@ -69,88 +53,29 @@ def read_model(filename):
     vertex_indices = [ind.vertex_index for ind in indices]
     normal_indices = [ind.normal_index for ind in indices]
 
-    if not attrib.normals:
+    vertices = [[vertices[3 * ind + j] for j in range(3)] for ind in vertex_indices]
+
+    if normals:
+        normals = [[normals[3 * ind + j] for j in range(3)] for ind in normal_indices]
+    else:
         normals = []
-        normal_indices = []
-        for i, inds in enumerate(group(vertex_indices, 3)):
-            mat = [[vertices[3 * ind + i] for i in range(3)] for ind in inds]
+        for i in range(0, len(vertices), 3):
+            mat = [vertices[i + j].copy() for j in range(3)]
             ab = np.subtract(mat[1], mat[0])
             ac = np.subtract(mat[2], mat[0])
-            normals.extend(normalize(np.cross(ab, ac)))
-            normal_indices.extend([i, i, i])
+            normals.extend([list(normalize(np.cross(ab, ac)))] * 3)
 
-    return vertices, normals, vertex_indices, normal_indices
-
-
-def is_event_quit(event):
-    is_key_esc = event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE
-    return event.type == pygame.QUIT or is_key_esc
-
-
-def redraw(vertices, normals, indices, n_indices):
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    draw(vertices, normals, indices, n_indices)
-    pygame.display.flip()
+    return vertices, normals
 
 
 def update_lights(lights_loc):
     t = time.time()
-    lights = [(sin(t * 2), cos(t * 2), 0, 1), (cos(t), 0, sin(t), 1), (0, sin(t * 3), cos(t * 3), 1)]
+    lights = [
+        [sin(t * 2), cos(t * 2), 0, 1],
+        [cos(t), 0, sin(t), 1],
+        [0, sin(t * 3), cos(t * 3), 1]
+    ]
     glUniform4fv(lights_loc, 3, lights)
-
-
-def main():
-    init()
-    program = create_program()
-    lights_loc = glGetUniformLocation(program, "lights")
-    animate_lights = True
-
-    vertices, normals, indices, n_indices = read_model("models/bunny.obj")
-
-    update_lights(lights_loc)
-    redraw(vertices, normals, indices, n_indices)
-
-    while True:
-        need_redraw = animate_lights
-        for event in pygame.event.get():
-            if is_event_quit(event):
-                pygame.quit()
-                quit()
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == SCROLL_UP:
-                    glScale(1 / ZOOM_FACTOR, 1 / ZOOM_FACTOR, 1 / ZOOM_FACTOR)
-                elif event.button == SCROLL_DOWN:
-                    glScale(ZOOM_FACTOR, ZOOM_FACTOR, ZOOM_FACTOR)
-                need_redraw = True
-            elif event.type == pygame.MOUSEMOTION and event.buttons[0]:
-                glMatrixMode(GL_MODELVIEW)
-                glRotate(ROTATION_FACTOR * event.rel[0], *UP_DIRECTION)
-                model_view_matrix = glGetDoublev(GL_MODELVIEW_MATRIX)
-                right = np.dot(model_view_matrix, RIGHT_DIRECTION_EXT)[:-1]
-                glRotate(ROTATION_FACTOR * event.rel[1], *right)
-                need_redraw = True
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                animate_lights ^= 1
-
-        if animate_lights:
-            update_lights(lights_loc)
-
-        if need_redraw:
-            redraw(vertices, normals, indices, n_indices)
-
-        pygame.time.wait(10)
-
-
-def init():
-    pygame.init()
-    pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), DOUBLEBUF | OPENGL)
-
-    glMatrixMode(GL_PROJECTION)
-    gluPerspective(FIELD_OF_VIEW, WINDOW_WIDTH / WINDOW_HEIGHT, Z_NEAR, Z_FAR)
-    gluLookAt(*CAMERA_POSITION, *SCENE_CENTER, *UP_DIRECTION)
-
-    glClearColor(*COLOR_BLACK)
-    glEnable(GL_DEPTH_TEST)
 
 
 def create_shader(shader_type, filename):
@@ -175,6 +100,90 @@ def create_program():
     glLinkProgram(program)
     glUseProgram(program)
     return program
+
+
+def init(vertices, normals):
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
+    glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT)
+    glutInit(sys.argv)
+    glutCreateWindow("3d-model")
+
+    glMatrixMode(GL_PROJECTION)
+    gluPerspective(FIELD_OF_VIEW, WINDOW_WIDTH / WINDOW_HEIGHT, Z_NEAR, Z_FAR)
+    gluLookAt(*CAMERA_POSITION, *SCENE_CENTER, *UP_DIRECTION)
+    glClearColor(*COLOR_BLACK)
+
+    glEnable(GL_DEPTH_TEST)
+    glEnableClientState(GL_NORMAL_ARRAY)
+    glEnableClientState(GL_VERTEX_ARRAY)
+    glVertexPointer(3, GL_FLOAT, 0, vertices)
+    glNormalPointer(GL_FLOAT, 0, normals)
+    glColor3f(1, 1, 1)
+
+    program = create_program()
+    lights_loc = glGetUniformLocation(program, "lights")
+    lights = [[1, 0, 0, 1], [0, 1, 0, 1], [0, 0, 1, 1]]
+    glUniform4fv(lights_loc, 3, lights)
+
+
+def on_draw_wrapper(n_verts):
+    def on_draw():
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glDrawArrays(GL_TRIANGLES, 0, n_verts)
+        glutSwapBuffers()
+    return on_draw
+
+
+# noinspection PyUnusedLocal
+def on_key_press(key, x, y):
+    if key == ESC_KEY:
+        exit(0)
+
+
+def on_mouse(button, state, x, y):
+    if button == GLUT_LEFT_BUTTON:
+        global prev_x, prev_y
+        if state == GLUT_DOWN:
+            prev_x = x
+            prev_y = y
+        else:
+            prev_x = prev_y = -1
+    elif button == SCROLL_UP and state == GLUT_DOWN:
+        glScale(ZOOM_FACTOR, ZOOM_FACTOR, ZOOM_FACTOR)
+        glutPostRedisplay()
+    elif button == SCROLL_DOWN and state == GLUT_DOWN:
+        glScale(1 / ZOOM_FACTOR, 1 / ZOOM_FACTOR, 1 / ZOOM_FACTOR)
+        glutPostRedisplay()
+
+
+def rotate(dx, dy):
+    glMatrixMode(GL_MODELVIEW)
+    glRotate(ROTATION_FACTOR * dx, *UP_DIRECTION)
+    model_view_matrix = glGetDoublev(GL_MODELVIEW_MATRIX)
+    right = np.dot(model_view_matrix, RIGHT_DIRECTION_EXT)[:-1]
+    glRotate(ROTATION_FACTOR * dy, *right)
+
+
+def on_mouse_move(x, y):
+    global prev_x, prev_y
+    if prev_x >= 0 and prev_y >= 0:
+        dx = x - prev_x
+        dy = y - prev_y
+        rotate(dx, dy)
+        prev_x = x
+        prev_y = y
+        glutPostRedisplay()
+
+
+def main():
+    vertices, normals = read_model("models/bunny.obj")
+    global prev_x, prev_y
+    init(vertices, normals)
+    glutDisplayFunc(on_draw_wrapper(len(vertices)))
+    glutMouseFunc(on_mouse)
+    glutMotionFunc(on_mouse_move)
+    glutKeyboardFunc(on_key_press)
+    glutMainLoop()
 
 
 if __name__ == "__main__":
